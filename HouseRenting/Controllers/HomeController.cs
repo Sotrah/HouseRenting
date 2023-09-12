0f41ab39df2f001661ca12e22c2f0f872f6494fc
359106 <s359106@oslomet.no>
2023-09-12 13:27:31 +0200

    .

--- HouseRenting/Controllers/HomeController.cs
+++ HouseRenting/Controllers/HomeController.cs
@@ -1,32 +1,19 @@
-﻿using HouseRenting.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HouseRenting.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ HouseRenting/Models/ItemDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HouseRenting.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRenting.Models;
+
+public class ItemDbContext : DbContext
+{
+    public ItemDbContext(DbContextOptions<ItemDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Item> Items { get; set; }
+}
