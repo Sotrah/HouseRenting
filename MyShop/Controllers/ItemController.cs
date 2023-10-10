50f55b7dec38ae84306ffb81d2e7321ebf4cbfa0
Caleb <89772184+belacmu@users.noreply.github.com>
2023-10-10 11:51:33 +0200

    Reset project, updated to myshop 14

--- MyShop/Controllers/ItemController.cs
+++ MyShop/Controllers/ItemController.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyShop.DAL;
 using MyShop.Models;
 using MyShop.ViewModels;
 
@@ -8,132 +8,121 @@ namespace MyShop.Controllers;
 
 public class ItemController : Controller
 {
-    private readonly ItemDbContext _itemDbContext;
+    private readonly IItemRepository _itemRepository;
+    private readonly ILogger<ItemController> _logger;
 
-    public ItemController(ItemDbContext itemDbContext)
+    public ItemController(IItemRepository itemRepository, ILogger<ItemController> logger)
     {
-        _itemDbContext = itemDbContext;
+        _itemRepository = itemRepository;
+        _logger = logger;
     }
 
-    public IActionResult Table()
+    public async Task<IActionResult> Table()
     {
-        List<Item> items = _itemDbContext.Items.ToList();
+        var items = await _itemRepository.GetAll();
+        if (items == null)
+        {
+            _logger.LogError("[ItemController] Item list not found while executing _itemRepository.GetAll()");
+            return NotFound("Item list not found");
+        }
         var itemListViewModel = new ItemListViewModel(items, "Table");
         return View(itemListViewModel);
     }
 
-    public IActionResult Grid()
+    public async Task<IActionResult> Grid()
     {
-        List<Item> items = _itemDbContext.Items.ToList();
+        var items = await _itemRepository.GetAll();
+        if (items == null)
+        {
+            _logger.LogError("[ItemController] Item list not found while executing _itemRepository.GetAll()");
+            return NotFound("Item list not found");
+        }
         var itemListViewModel = new ItemListViewModel(items, "Grid");
         return View(itemListViewModel);
     }
 
-    public IActionResult Details(int id)
+    public async Task<IActionResult> Details(int id)
     {
-        List<Item> items = _itemDbContext.Items.ToList();
-        var item = items.FirstOrDefault(i => i.ItemId == id);
+        var item = await _itemRepository.GetItemById(id);
         if (item == null)
-            return NotFound();
+        {
+            _logger.LogError("[ItemController] Item not found for the ItemId {ItemId:0000}", id);
+            return NotFound("Item not found for the ItemId");
+        }
         return View(item);
     }
 
-    //public IActionResult Table()
-    //{
-    //    var items = GetItems();
-    //    ViewBag.CurrentViewName = "Table";
-    //    return View(items);
-    //}
-
-    //public IActionResult Grid()
-    //{
-    //    var items = GetItems();
-    //    ViewBag.CurrentViewName = "Grid";
-    //    return View(items);
-    //}
-
-    public List<Item> GetItems()
+    [HttpGet]
+    [Authorize]
+    public IActionResult Create()
     {
-        var items = new List<Item>();
-        var item1 = new Item
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Create(Item item)
+    {
+        if (ModelState.IsValid)
         {
-            ItemId = 1,
-            Name = "Hus1",
-            Price = 150,
-            Description = "Hus1.",
-            ImageUrl = "/images/hus1.jpg",
-            Rooms = 1,
-            Address = "Address1",
-            Telephone = 1111111
-        };
+            bool returnOk = await _itemRepository.Create(item);
+            if (returnOk)
+                return RedirectToAction(nameof(Table));
+        }
+        _logger.LogWarning("[ItemController] Item creation failed {@item}", item);
+        return View(item);
+    }
 
-        var item2 = new Item
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> Update(int id)
+    {
+        var item = await _itemRepository.GetItemById(id);
+        if (item == null)
         {
-            ItemId = 2,
-            Name = "Hus 2",
-            Price = 20,
-            Description = "Hus2.",
-            ImageUrl = "/images/hus2.jpg",
-            Rooms = 2,
-            Address = "Address2",
-            Telephone = 2222222
-        };
+            _logger.LogError("[ItemController] Item not found when updating the ItemId {ItemId:0000}", id);
+            return BadRequest("Item not found for the ItemId");
+        }
+        return View(item);
+    }
 
-        var item3 = new Item
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Update(Item item)
+    {
+        if (ModelState.IsValid)
         {
-            ItemId = 3,
-            Name = "Hus3",
-            Price = 50,
-            Description = "Hus3",
-            ImageUrl = "/images/hus3.jpg",
-            Rooms = 3,
-            Address = "Address3",
-            Telephone = 3333333
-        };
+            bool returnOk = await _itemRepository.Update(item);
+            if (returnOk)
+                return RedirectToAction(nameof(Table));
+        }
+        _logger.LogWarning("[ItemController] Item update failed {@item}", item);
+        return View(item);
+    }
 
-        var item4 = new Item
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var item = await _itemRepository.GetItemById(id);
+        if (item == null)
         {
-            ItemId = 4,
-            Name = "Hus4",
-            Price = 250,
-            Description = "Hus4",
-            ImageUrl = "/images/hus4.jpg",
-            Rooms = 4,
-            Address = "Address4",
-            Telephone = 44444444
-        };
+            _logger.LogError("[ItemController] Item not found for the ItemId {ItemId:0000}", id);
+            return BadRequest("Item not found for the ItemId");
+        }
+        return View(item);
+    }
 
-        var item5 = new Item
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        bool returnOk = await _itemRepository.Delete(id);
+        if (!returnOk)
         {
-            ItemId = 5,
-            Name = "Hus5",
-            Price = 150,
-            Description = "Hus5",
-            ImageUrl = "/images/hus5.jpg",
-            Rooms = 5,
-            Address = "Address5",
-            Telephone = 5555555
-        };
-
-        var item6 = new Item
-        {
-            ItemId = 6,
-            Name = "Hus6",
-            Price = 180,
-            Description = "Hus6",
-            ImageUrl = "/images/hus6.jpg",
-            Rooms = 6,
-            Address = "Address6",
-            Telephone = 6666666
-        };
-
-
-        items.Add(item1);
-        items.Add(item2);
-        items.Add(item3);
-        items.Add(item4);
-        items.Add(item5);
-        items.Add(item6);
-        return items;
+            _logger.LogError("[ItemController] Item deletion failed for the ItemId {ItemId:0000}", id);
+            return BadRequest("Item deletion failed");
+        }
+        return RedirectToAction(nameof(Table));
     }
 }
--- MyShop/Models/Item.cs
+++ MyShop/Models/Item.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace MyShop.Models
 {
-	public class Item
-	{
-		public int ItemId { get; set; }
-		public string Name { get; set; } = string.Empty;
-		public decimal Price { get; set; }
-		public string? Description { get; set; }
-		public string? ImageUrl { get; set; }
-		public int? Rooms { get; set; }
-        public string? Address { get; set; }
-        public int? Telephone { get; set; }
-	}
+    public class Item
+    {
+        public int ItemId { get; set; }
+
+        [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,20}", ErrorMessage = "The Name must be numbers or letters and between 2 to 20 characters.")]
+        [Display(Name = "Item name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "The Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        [StringLength(200)]
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+        // navigation property
+        public virtual List<OrderItem>? OrderItems { get; set; }
+    }
 }
 
