f9e1e0ce594d063ff45e56024179f1641d3347b8
Caleb <89772184+belacmu@users.noreply.github.com>
2023-10-18 10:42:58 +0200

    Added incomplete functionality for creating new Bookings from the Item Details page
    Right now the datepicker isn't communicating properly to createBooking

---
+++ MyShop/Controllers/BookingController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using Microsoft.EntityFrameworkCore;
+using MyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using MyShop.DAL;
+namespace MyShop.Controllers;
+
+public class BookingController : Controller
+{
+    private readonly ItemDbContext _itemDbContext;
+
+    public BookingController(ItemDbContext itemDbContext)
+    {
+        _itemDbContext = itemDbContext;
+    }
+
+    public async Task<IActionResult> Table()
+    {
+        List<Booking> bookings = await _itemDbContext.Bookings.ToListAsync();
+        return View(bookings);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> CreateBooking(Booking booking)
+    {
+        try
+        {
+            var newItem = await _itemDbContext.Items.FindAsync(booking.ItemId);
+
+            if (newItem == null)
+            {
+                return BadRequest("Item not found.");
+            }
+
+            var newBooking = new Booking
+            {
+                BookingDate = booking.BookingDate,
+                ItemId = booking.ItemId,
+                Item = newItem,
+            };
+
+            _itemDbContext.Bookings.Add(newBooking);
+            await _itemDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Table));
+        }
+        catch
+        {
+            return BadRequest("BookingItem creation failed.");
+        }
+    }
+}
